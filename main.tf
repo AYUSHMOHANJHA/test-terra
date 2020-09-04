2d08b52801b4bbc64bdd47988d4f2dda168eb2b4
Ayush Mohan Jha <AYUSHJHA06@GMAIL.COM>
2020-09-04 10:24:12 +0530

    Web Resource Added

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 provider "aws"{
   region  = "us-east-1"
+}
+resource "aws_instance" "web" {
+  ami           = "ami-0625a212c50aa6656"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
